43202b7bbb1828f5ff9f5da052977111f2a9018f
s3877698 <s3877698@rmit.edu.vn>
2021-07-25 10:21:52 +0700

    get data from csv

---
+++ src/Data.java
@@ -0,0 +1,5 @@
+public class Data {
+    private String area;
+    private String startDate;
+    private String endDate;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
+        Scanner sc = new Scanner(new File("src/data.csv"));
+        ArrayList<String> dataByRow = new ArrayList<String>();
+
+        while (sc.hasNextLine()) {
+            dataByRow.add(sc.nextLine());
+        }
+
+        printArrayList(dataByRow);
+        System.out.println(dataByRow.get(0));
     }
+
+    public static void printArrayList(ArrayList<String> arrayList) {
+        for (String row : arrayList) {
+            System.out.println(row);
+        }
+    }
+
+
 }
