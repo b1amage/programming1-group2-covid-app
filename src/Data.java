e2da5fd79c3c98c820411c2a81a52e1093a18ae8
s3877698 <s3877698@rmit.edu.vn>
2021-07-27 12:22:34 +0700

    put data to an array list<HashMap>

--- src/Data.java
+++ src/Data.java
@@ -2,4 +2,34 @@ public class Data {
     private String area;
     private String startDate;
     private String endDate;
+
+    public Data(String area, String startDate, String endDate) {
+        this.area = area;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
 }
