fe69cd3f302863375fe224733fb83f8acaf01c43
TuanDao2002 <s3877347@rmit.edu.vn>
2021-08-12 10:46:03 +0700

    add display

--- src/Display.java
+++ src/Display.java
@@ -1,5 +1,22 @@
-public class Display {
-    public static void tabularDisplay() {
+import java.io.IOException;
 
+public class Display {
+    public static void tabularDisplay(Data data) {
+        System.out.println("Range" + "\t\t\t\t" + "Value");
+
+        for (Row row : data.getRowsFromStartDate()) {
+            System.out.println(row.getDate() + "\t\t\t" + row.getNewCases() + 100000);
+        }
+    }
+
+    public static void chartDisplay(Data data) {
+        char[][] chart = new char[24][80];
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        Data data = Data.createData();
+        data.createRowData();
+        tabularDisplay(data);
     }
 }
