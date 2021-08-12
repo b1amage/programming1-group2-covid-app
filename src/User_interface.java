10e6e97f67c43dd548b77787a6b72dba67dcedf7
s3878694 <s3878694@rmit.edu.vn>
2021-08-12 16:52:33 +0700

    update + clean code

--- src/User_interface.java
+++ src/User_interface.java
@@ -1,12 +1,21 @@
 import java.io.IOException;
+import java.util.Scanner;
+
 
 public class User_interface {
     public static void main(String[] args) throws IOException {
         boolean isRunning = true;
+        Scanner sc = new Scanner(System.in);
         while (isRunning){
             Data data = Data.createData();
             data.createRowData();
             DataProcess.displayRows(data.getRowsFromStartDate());
+            System.out.println("Do you wish to conitnue \n (1) Yes \n (2) No");
+            int cont = sc.nextInt();
+            isRunning = (1 == cont);
+
+
+
         }
     }
 }
