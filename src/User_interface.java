2b93e01efeac0c0d30ad1c7fa7bd35d8cdcab89b
s3878694 <s3878694@rmit.edu.vn>
2021-08-14 18:46:31 +0700

    update user interface

--- src/User_interface.java
+++ src/User_interface.java
@@ -10,6 +10,15 @@ public class User_interface {
             Data data = Data.createData();
             data.createRowData();
             DataProcess.displayRows(data.getRowsFromStartDate());
+            System.out.println("(1) Chart display \n (2) Tabular display");
+            int displayMethod = sc.nextInt();
+            if (displayMethod == 1){
+                //Chart display
+            } else if (displayMethod == 2){
+                //Tabular display
+            };
+
+            // Ask the user if they want to continue
             System.out.println("Do you wish to conitnue \n (1) Yes \n (2) No");
             int cont = sc.nextInt();
             isRunning = (1 == cont);
