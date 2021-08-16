11908d839ec1af0717138fee16b81162e4a31a01
TuanDao2002 <s3877347@rmit.edu.vn>
2021-08-16 20:57:37 +0700

    fix conflict

--- src/StartAndEndDate.java
+++ src/StartAndEndDate.java
@@ -1,5 +1,4 @@
 public class StartAndEndDate extends TimeRange{
-
     public StartAndEndDate(String startDate, String endDate, int nextDayCount) {
         super(startDate, endDate, nextDayCount);
     }
