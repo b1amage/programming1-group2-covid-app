a69521f9c67ea93a45b6ff5a198920b9b2cbf70f
s3878694 <s3878694@rmit.edu.vn>
2021-08-24 23:14:58 +0700

    add interface

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        ArrayList<Row> rows = DataProcess.createRowList();
-
-        DataProcess.displayRows(rows);
+    public static void main(String[] args) throws IOException {
+        UserInterface userInterface = new UserInterface();
+        userInterface.displayUI();
     }
 }
