e2ae48a70ab3423a969ccf983a06ce0cdde81068
s3877698 <s3877698@rmit.edu.vn>
2021-08-28 11:47:03 +0700

    update docstring

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+/*
+  Class: Main
+  Purpose: Run client side code
+  Contributors: Minh Long, Quoc Bao, Kha Tuan, Anh Duy
+  Created date: 10/8/2021
+  Last modified: 28/8/2021
+  Version 1.0
+ */
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         UserInterface userInterface = new UserInterface();
         userInterface.displayUI();
-
     }
 }
